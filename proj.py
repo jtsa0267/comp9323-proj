680b798a3c7c598163e12c6cf0723d25a7238537
jtsa0267 <jtsa0267@gmail.com>
2018-08-28 04:01:00 +1000

    added quantity filter to trim down number of ingredients

--- proj.py
+++ proj.py
@@ -16,11 +16,6 @@ def get_recipes():
     from requests import get
     from time import time
 
-    def revert_cooking_abbr(ing_str: str):
-        return ing_str.replace("tsp", "teaspoon")\
-                      .replace("tbsp", "tablespoon")\
-                      .replace("tbs", "tablespoon")
-
     def get_openrecipes():
         from os import chdir, remove
         from shutil import copyfileobj
@@ -57,13 +52,13 @@ def get_recipes():
                  "prepTime" : "P",
                  "image" : chowdown_base_url + soup1.find_all("img")[0].attrs["src"]}
             tag_filter = {"class" : "sm-col-8 center mx-auto"}
-            remove_descr_hyperlink = BeautifulSoup(str(soup1.find("div", tag_filter).p), "lxml")
-            for a in remove_descr_hyperlink.findAll('a'):
+            remove_hyperlink = BeautifulSoup(str(soup1.find("div", tag_filter).p), "lxml")
+            for a in remove_hyperlink.findAll('a'):
                 a.replaceWithChildren()
-            d["description"] = "".join(remove_descr_hyperlink.p.contents)
+            d["description"] = "".join(remove_hyperlink.p.contents)
             tag_filter = {"itemprop" : "ingredient"}
-            d["ingredients"] = "\n".join([revert_cooking_abbr(ing.p.contents[0])
-                                         for ing in soup1.find_all("li", tag_filter)])
+            d["ingredients"] = "\n".join([ing.p.contents[0]
+                                          for ing in soup1.find_all("li", tag_filter)])
             with open(resdir + fname, "a") as f:
                 f.write(str(d).replace("'", "\"") + "\n")
 
@@ -74,17 +69,49 @@ def get_recipes():
 
 def get_ingredients():
     from json import loads
+    from nltk.tag import pos_tag
     from os import listdir
     from os.path import isfile
+    import re
 
+    units = ""
+    with open(resdir + "units", "r") as f:
+        units = "(" + "|".join([line.rstrip() for line in f]) + ")"
+    num_frac = "[\u2189\u2150-\u215f\u00bc-\u00be\d\.\-\–\/]"
+    quantity_filter = "([\d]+\s*x\s+)*(((" + num_frac + "|\s+to\s+)\s*)+("+ units + "(\s+(of\s+)*|\s*[\,\.\-\–\/]s*))*)+"
+    l_ing = set()
     for fname in listdir(resdir):
         if not isfile(resdir + fname) or not fname.endswith(".json"):
             continue
         with open(resdir + fname) as f:
-            content = [loads(l) for l in f.readlines()]
-            print(len(content))
+            for line in f.readlines():
+                for ing in loads(line)["ingredients"].lower().strip().split("\n"):
+                    # below is a sequence of filters
+                    ing = re.sub("\(.*?\)", "", ing) # removes between ()
+                    ing = re.sub("\(.*", "", ing) # removes ( and after
+                    ing = re.compile(r"" + quantity_filter, flags = re.IGNORECASE).sub("", ing)
+                    ing = re.sub("^\s*" + units + "\s*$", "", ing)
+                    ing = re.sub("^\s*(x|X)\s+", "", ing)
+                    l_ing.add(ing.strip())
+
+    l_ing2 = set()
+    for ing in l_ing:
+        for elem in ing.split(","):
+            for elem1 in elem.split(" or "):
+                for elem2 in elem1.split(" and "):
+                    for elem3 in elem2.split(" of "):
+                        for elem4 in elem3.split(" for "):
+                            for elem5 in elem4.split(";"):
+                                for tag in pos_tag(elem5.split()):
+                                    if re.match("NN((S|PS*))*", tag[1]):
+                                        l_ing2.add(elem5.strip())
+    print(len(l_ing2))
+    # print(l_ing2)
 
 if __name__ == '__main__':
     get_recipes()
     get_ingredients()
     app.run()
+
+# nltk.download('averaged_perceptron_tagger')
+# 129112
