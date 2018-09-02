028d668f355c2f33c0d1422141ec2d0851d054e1
jtsa0267 <jtsa0267@gmail.com>
2018-09-02 18:03:53 +1000

    improved filter and dictionary

--- proj.py
+++ proj.py
@@ -69,52 +69,79 @@ def get_recipes():
 
 def get_ingredients():
     from json import loads
+    from nltk import PorterStemmer
     from nltk.tag import pos_tag
+    from nltk.tokenize import word_tokenize
     from os import listdir
     from os.path import isfile
+    from symspellpy.symspellpy import SymSpell, Verbosity
     import re
 
-    units = ""
+    def symspell_correction(ing_str):
+        sym_spell = SymSpell(83000, 2)
+        dictionary_path = dirname(realpath(__file__)) + "/symspellpy/frequency_dictionary_en_82_765.txt"
+        if not sym_spell.load_dictionary(dictionary_path, 0, 1):
+            return ""
+        suggestions = sym_spell.lookup_compound(ing_str, 1)
+        return sorted(suggestions, key = lambda x: x.count, reverse = False)[0].term
+
+    units_regex, units_set, all_set, l_ing = "", set(), set(), set()
     with open(resdir + "units", "r") as f:
-        units = "(" + "|".join([line.rstrip() for line in f]) + ")"
-    num_frac = "[\u2215\u2189\u2150-\u215f\u2044\u2010-\u2015\u00bc-\u00be\u002d-\u0039]"
-    quantity_filter = "([\d]+\s*x\s+)*(((" + num_frac\
-                                           + "|\s+to\s+)\s*)+("\
-                                           + units\
-                                           + "(\s+(of\s+)*|\s*[\u2215\u2044\u2010-\u2015\u002c-\u002f]\s*))*)+"
-    l_ing = set()
+        tmp = [line.rstrip() for line in f]
+        units_regex = re.sub("(\.|\#)", r"\\\1", "|".join(tmp))
+        units_set = set(tmp)
+    with open(resdir + "states", "r") as f:
+        all_set = set([line.rstrip() for line in f]).union(units_set)
+    quantity_filter = "[\u2150-\u215e\u00bc-\u00be\u0030-\u0039]\s*("\
+                          + units_regex + ")*(\s*\)\s*of\s+|\s+of\s+|\s*\)\s*|\s+)"\
+                          + "([\u24C7\u2122\u00c0-\u00d6\u00d8-\u00f6\u00f8-\u01bf\u01cd-\u02af\u0061-\u007a\ \-]{2,})"
+    descriptor_filter = "(^|\s+)[a-z]+([\u002d\u2010-\u2015][a-z]+)+($|\s+)"
     for fname in listdir(resdir):
         if not isfile(resdir + fname) or not fname.endswith(".json"):
             continue
         with open(resdir + fname) as f:
-            for line in f.readlines():
-                for ing in loads(line)["ingredients"].lower().strip().split("\n"):
-                    # below is a sequence of filters
-                    ing = re.sub("\(.*?\)", "", ing) # removes between ()
-                    ing = re.sub("\(.*", "", ing) # removes ( and after
-                    ing = re.compile(r"" + quantity_filter, flags = re.IGNORECASE).sub("", ing)
-                    ing = re.sub("^\s*" + units + "\s*$", "", ing)
-                    ing = re.sub("^\s*(x|X)\s+", "", ing)
-                    l_ing.add(ing.strip())
+            for k, line in enumerate(f.readlines()):
+                print(k)
+                for ing in re.split("\n|,", loads(line)["ingredients"].lower().strip()):
+                    ing = re.findall(quantity_filter, ing)
+                    if not ing or len(ing) > 1:
+                        continue
+                    # ing = re.sub("(^|\s+)[a-z]+\-[a-z]+($|\s+)", " ", ing)
+                    # ing = symspell_correction(re.sub("^\s*x\s+", "", ing[0][-1].strip()))
+                    ing = re.sub("^\s*(x|X)\s+", "", ing[0][-1].strip())
+                    if not ing:
+                        continue
+                    for elem in re.split("\s+(and|or|with|in)\s+", ing):
+                        s, j, tmp = "", -1, pos_tag(word_tokenize(elem))
+                        for i, tag in enumerate(tmp):
+                            if re.match("NNS*", tag[1]) and tag[0] not in all_set:
+                                j = i
+                                break
+                        if j != -1:
+                            if j != 0 and tmp[j - 1][1] == "JJ" and tmp[j - 1][0] not in all_set\
+                                      and not re.match(descriptor_filter, tmp[j - 1][0][0]):
+                                s = tmp[j - 1][0]
+                            while tmp[j][1] == "NN" or tmp[j][1] == "NNS":
+                                if tmp[j][0] in all_set or re.match(descriptor_filter, tmp[j][0]):
+                                    j += 1
+                                    if j == len(tmp):
+                                        break
+                                    else:
+                                        continue
+                                s += " " + str(PorterStemmer().stem(tmp[j][0]))
+                                j += 1
+                                if j == len(tmp):
+                                    break
+                        if s.strip() and len(s.strip()) > 1:
+                            l_ing.add(s.strip())
 
-    l_ing2 = set()
-    for ing in l_ing:
-        for elem in re.split("\u0021|\u0025|[\u002a-\u002c\u003a-\u003b]|\u007e|\s+(or|and|of|for)\s+|&amp", ing):
-            # TODO add spelling corrections to ingredients before removing non-NN's
-            if not elem:
-                continue
-            s = ""
-            for tag in pos_tag(elem.split()):
-                if re.match("NNS*", tag[1]) and not re.match("^" + units + "$", tag[0]):
-                    s += tag[0] + " "
-            l_ing2.add(s.strip())
-    print(l_ing2)
-    print(len(l_ing2))
+    print(l_ing)
+    print(len(l_ing))
 
 if __name__ == '__main__':
     get_recipes()
     get_ingredients()
     app.run()
 
-# nltk.download('averaged_perceptron_tagger')
-# 54837
+# nltk.download('punkt')
+# 23488
