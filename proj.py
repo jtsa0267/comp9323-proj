b1957fee898dde36fdb1bffb0974396fa3157cee
vmak19 <v.mak@hotmail.com>
2018-10-17 22:49:09 +1100

    Done: create user & edit user profile details

--- proj.py
+++ proj.py
@@ -20,15 +20,15 @@ def greet():
     return 'Hi, you are not logged in'
 
 '''Signs user in and redirects to homepage'''
-@app.route('/login', methods=['POST', 'GET'])
+@app.route('/login', methods=['POST'])
 def login():
     if request.method == 'POST':
-        username = request.form['username']
-        password = request.form['password']
+        username = request.get_json()['email']
+        password = request.get_json()['password']
         db = connect_db()
         res = db.users.find_one({"username": username})
         if password==res['password']:
-            session['username'] = request.form['username']
+            session['username'] = username
             return json.dumps({'success': True}), 200, {'ContentType': 'application/json'}
     return json.dumps({'success': False,'error': 'Wrong credentials'}), 200, {'ContentType': 'application/json'}
 
@@ -110,48 +110,97 @@ def insert_db_recipes():
             db.recipes.insert(json.loads(row))
 
 def get_ingredient_refence():
-    from re import match, sub
     from textblob.inflect import singularize
+
+    def oxford_reference_ing():
+        from re import match, sub
+
+        oxfordreference_base_url = "http://www.oxfordreference.com/view/10.1093/acref/9780199234875.001.0001/acref-9780199234875"
+        tag_filter = {"class" : "contentItem oxencycl-entry locked hasCover chunkResult hi-visible p-4 border-top"}
+        i, ing_list = 1, []
+        while True:
+            url = oxfordreference_base_url + "?page=" + str(i) + "&pageSize=100"
+
+            print(url)
+
+            soup = BeautifulSoup(get(url).text, "lxml")
+            l = soup.find_all("div", tag_filter)
+            if not l:
+                break
+            for ing_div in l:
+                content_list = ing_div.h2.a.contents
+                if len(content_list) == 1:
+                    content_split = content_list[0].split(",")
+                    ings = []
+                    if len(content_split) > 2:
+                        for content in content_split:
+                            ings.append(content.strip())
+                    elif len(content_split) == 2:
+                        ings.append(content_split[1].strip() + " " + content_split[0].strip())
+                    else:
+                        ings.append(content_split[0].strip())
+                    for ing in ings:
+                        if len(ing) > 2 and not ing.startswith("free ") and not ing.startswith("food ") and\
+                        not match("^.*[A-Z].*$", ing) and not match("^.*[\u2010-\u2015\-]$", ing):
+                            ing = sub("\(.*\)", "", ing).strip()
+                            if not ing:
+                                continue
+                            ing_split = ing.split(" ")
+                            ing_list.append((' '.join(ing_split[: -1]) + " " + singularize(ing_split[-1])).strip())
+            i += 1
+
+        return ing_list
+
+    def wiki_cookbook_ing():
+        from bs4 import Comment
+        from re import sub
+
+        def ul_children(sib):
+            l = []
+            if not sib.name == "ul":
+                if sib.name:
+                    l.append(sib.a.contents[0])
+            else:
+                for child in sib.findAll(["li", "ul"]):
+                    l += ul_children(child)
+
+            return l
+
+        wikicookbook_url = "https://en.wikibooks.org/wiki/Cookbook:Ingredients"
+        soup = BeautifulSoup(get(wikicookbook_url).text, "lxml")
+        l, ing_list = soup.find_all("h2"), set()
+        for title_tag in l:
+            if not title_tag.span:
+                continue
+            for sibling in title_tag.next_siblings:
+                if sibling.name == "h2" or isinstance(sibling, Comment):
+                    break
+                l_alphabet_ing = ul_children(sibling)
+                if l_alphabet_ing:
+                    for ing in l_alphabet_ing:
+                        ing = sub("\(.*\)", "", ing).strip()
+                        if ing.startswith("Dairy products and ") or ing.endswith(" family"):
+                            continue
+                        ing_words = ing.split(",")
+                        if len(ing_words) > 1:
+                            ing = ing_words[1].strip() + " " + ing_words[0].strip()
+                        else:
+                            ing = ing_words[0].strip()
+                        ing_words = ing.split(" ")
+                        ing_list.add((" ".join(ing_words[: -1]) + " " + singularize(ing_words[-1])).lower().strip())
+
+        return list(ing_list)
 
     fname = "ing_list"
     if isfile(resdir + fname):
         return
-    oxfordreference_base_url = "http://www.oxfordreference.com/view/10.1093/acref/9780199234875.001.0001/acref-9780199234875"
-    tag_filter = {"class" : "contentItem oxencycl-entry locked hasCover chunkResult hi-visible py-3 border-top flex flex-row"}
-    i, ing_list = 1, []
-    while True:
-        soup = BeautifulSoup(get(oxfordreference_base_url + "?page=" + str(i) + "&pageSize=100").text, "lxml")
-        l = soup.find_all("div", tag_filter)
-        if not l:
-            break
-        for ing_div in l:
-            content_list = ing_div.h2.a.contents
-            if len(content_list) == 1:
-                content_split = content_list[0].split(",")
-                ings = []
-                if len(content_split) > 2:
-                    for content in content_split:
-                        ings.append(content.strip())
-                elif len(content_split) == 2:
-                    ings.append(content_split[1].strip() + " " + content_split[0].strip())
-                else:
-                    ings.append(content_split[0].strip())
-                for ing in ings:
-                    if len(ing) > 2 and not ing.startswith("free ") and not match("^.*[A-Z].*$", ing) and\
-                    not match("^.*[\u2010-\u2015\-]$", ing):
-                        ing = sub("\(.*\)", "", ing).strip()
-                        if not ing:
-                            continue
-                        ing_split = ing.split(" ")
-                        ing_list.append(' '.join(ing_split[: -1]) + " " + singularize(ing_split[-1]))
-        i += 1
+    l_ing = list(set(oxford_reference_ing() + wiki_cookbook_ing()))
     with open(resdir + fname, "w") as f:
-        for t in sorted(ing_list):
+        for t in sorted(l_ing):
             f.writelines(t + "\n")
 
 #Scrape several websites for recipes
 def get_recipes():
-    from bs4 import BeautifulSoup
     from datetime import datetime
     from os import makedirs
     from time import time
@@ -208,10 +257,9 @@ def get_recipes():
 def scrape_ingredients():
     from json import loads
     from nltk.corpus import wordnet
-    from nltk.tokenize import word_tokenize
     from os import listdir
     from os.path import isfile
-    from textblob import TextBlob
+    from textblob.inflect import singularize
     import re
 
     def symspell_correction(misspelled):
@@ -230,7 +278,7 @@ def scrape_ingredients():
 
     units_regex, units_set, l_ing, ings = "", set(), set(), set()
     with open(resdir + "units", "r") as f:
-        tmp = [line.rstrip() for line in f]
+        tmp = [line.strip() for line in f]
         units_regex = re.sub("(\.|\#)", r"\\\1", "|".join(tmp))
         units_set = set(tmp)
     quantity_filter = "[\u2150-\u215e\u00bc-\u00be\u0030-\u0039]\s*("\
@@ -243,38 +291,35 @@ def scrape_ingredients():
             continue
         with open(resdir + fname, encoding='utf-8') as f:
             for k, line in enumerate(f.readlines()):
-                print(k)
-                for ing in re.split("\n|,", loads(line)["ingredients"].lower().strip()):
-                    ing = re.findall(quantity_filter, ing)
-                    if not ing or len(ing) > 1:
+                # print(k)
+                for ing_str in re.split("\n|,", loads(line)["ingredients"].lower().strip()):
+                    ing_str = re.findall(quantity_filter, ing_str)
+                    if not ing_str or len(ing_str) > 1:
                         continue
-                    for elem in re.split("\s+(and|or|with|in)\s+", ing[0][-1].strip()):
-                        s, tokens = "", reversed(word_tokenize(elem))
-                        for i, token in enumerate(tokens):
-                            token = token.rstrip()
+                    # maybe split on stopwords
+                    for ing_str_split in re.split("\s+(and|or|with|in)\s+", ing_str[0][-1].strip()):
+                        ing, ing_str_tokens = "", list(reversed(ing_str_split.split(" ")))
+                        for i, token in enumerate(ing_str_tokens):
+                            token = token.strip()
                             if not token or re.match("^[a-z]+([\u002d\u2010-\u2015][a-z]+)+$", token):
                                 continue
                             # if token not in ings and not wordnet.synsets(token):
                             #     token = symspell_correction(token)
-                            if i == 0:
-                                token = TextBlob(token).words
-                                if not token:
-                                    continue
-                                token = token[0].singularize()
+                            if not ing:
+                                token = singularize(token)
                             if token not in ings or token in units_set:
                                 continue
-                            else:
-                                s = token
-                            for j in range(i + 1, len(list(tokens))):
-                                tmp = " ".join(reversed(list(tokens)[i + 1 : j + 1]))
-                                if not tmp + " " + s in ings:
-                                    s = " ".join(reversed(list(tokens)[i + 1 : j])) + " " + s
+                            ing = token
+                            for j in range(i + 1, len(ing_str_tokens)):
+                                tmp = " ".join(reversed(ing_str_tokens[i + 1 : j + 1]))
+                                if tmp + " " + ing not in ings:
+                                    ing = " ".join(reversed(ing_str_tokens[i + 1 : j])) + " " + ing
                                     break
                             break
-                        s = s.strip()
-                        if s and len(s) > 2:
-                            l_ing.add(s.strip())
-    print(l_ing)
+                        ing = ing.strip()
+                        if len(ing) > 2:
+                            l_ing.add(ing.strip())
+    print(sorted(list(l_ing)))
     print(len(l_ing))
 
 ''' Returns all ingredients from textfile as JSON'''
@@ -289,22 +334,70 @@ def get_ingredients():
     return jsonify(json_rows)
 
 # Inserts/updates user into database
-@app.route("/users/<username>", methods=['Get'])
-def insert_user(username):
+'''
+POST - creates new user
+PUT - updates user details.
+'''
+@app.route("/users", methods=['POST','PUT'])
+def handle_user():
     db = connect_db()
-    db.users.insert("")
-    abort(400, 'API not fully implemented yet ):')
+    sc=1
 
+
+    if request.json is None:
+        abort(400, 'No valid JSON not provided')
+    elif request.method == 'POST':
+        email = request.get_json()['email']
+        password = request.get_json()['password']
+        fName = request.get_json()['fname']
+        lName = request.get_json()['lname']
+        sc=db.users.insert({"username":email,"password":password,"first_name":fName,"last_name":lName})
+    elif request.method == 'PUT':
+        if not 'username' in session:
+            return json.dumps({'success': False,'error':"You need to be logged in first."}), 401, {'ContentType': 'application/json'}
+        currEmail = session['username']
+        email = request.get_json()['email']
+        password = request.get_json()['password']
+        fName = request.get_json()['fname']
+        lName = request.get_json()['lname']
+
+        query = {}
+        if email:
+            query["username"] = email
+        if password:
+            query["password"] = password
+        if fName:
+            query["first_name"] = fName
+        if lName:
+            query["last_name"] = lName
+
+        '''
+        query is in format of:
+         {
+            'username':email,
+            'password': password,
+            'first_name': fName,
+            'last_name': lName
+        }
+        '''
+        sc=db.users.update(
+            {'username': currEmail},
+            query
+        )
+
+    if sc:
+        return json.dumps({'success': True}), 200, {'ContentType': 'application/json'}
+    else:
+        return json.dumps({'success': False}), 401, {'ContentType': 'application/json'}
 
 if __name__ == '__main__':
     if not exists(resdir):
         import os
         os.makedirs(resdir)
     get_ingredient_refence()
-    exit()
+    # exit()
     get_recipes()
     scrape_ingredients()
     connect_db()
     app.run()
-# nltk.download('punkt')
-# 1137
+# 1298
